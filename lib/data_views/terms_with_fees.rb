0c3237c9c60e026eabc5844a05a0c50c26b0f1cb
Ian Whitney <whit0694@umn.edu>
2016-06-22 10:27:01 -0500

    Add a view to identify terms that have fees
    This query comes from the existing [One Stop Course
    Fees](https://github.umn.edu/asrweb/one_stop_course_fees) project. It is
    used to determine what terms currently have fees in TFMS.
    
    We will be using this to determine what terms to return for each campus.

---
+++ lib/data_views/terms_with_fees.rb
@@ -0,0 +1,44 @@
+=begin
+Term data for terms that have course fees.
+=end
+module DataViews
+  class TermsWithFees < ViewBuilder::View
+    def self.view_name
+      "terms_with_fees"
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def self.definition_sql
+      <<~SQL
+        SELECT
+          strm
+        FROM (
+          SELECT
+            DISTINCT CASE
+              when fall = 1 then
+                (fiscal_years.year - 1) - 1900 || '9'
+              when spring = 1 then
+                fiscal_years.year - 1900 || '3'
+              when summer = 1 then
+                fiscal_years.year -1900 || '5'
+            end as strm
+          FROM
+            asr_tfms.courses
+          INNER JOIN
+            asr_tfms.fee_occurrences
+          ON
+            courses.fee_occurrence_id=fee_occurrences.id
+          LEFT JOIN
+            asr_tfms.fiscal_years
+          ON
+            fee_occurrences.fiscal_year_id=fiscal_years.id
+        ) term_codes
+        WHERE
+          term_codes.strm is not null
+        ORDER BY
+          strm
+      SQL
+    end
+    # rubocop:enable Metrics/MethodLength
+  end
+end
