fcf49c8cb540b7a60c483792be34cf13fe083c3a
Ian Whitney <whit0694@umn.edu>
2016-09-07 08:41:30 -0500

    Deploy to new rhel6 server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,21 @@
-# Do not deploy to staging.
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don"t declare `role :all`, it"s a meta role
+# role :app, %w{deploy@example.com}
+role :app, %w(asr-coursefees-qat-web-02.oit.umn.edu)
+role :web, %w(asr-coursefees-qat-web-02.oit.umn.edu)
+
+# Configuration
+set :user, "swadm"
+set :server, "asr-coursefees-qat-web-02.oit.umn.edu"
+set :roles, %w(web app)
+set :web_root, "/swadm/www/"
+
+server(
+  "asr-coursefees-qat-web-02.oit.umn.edu",
+  roles: fetch(:roles),
+  web_root: fetch(:web_root)
+)
