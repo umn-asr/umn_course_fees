630bc0b048762e3b01fb2fe5c882e4485a8b3bf9
Tony Thomas <thoma127@umn.edu>
2021-05-26 15:59:11 -0500

    Change deploy config to point to new servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, "production"
 
-server "asr-coursefees-prd-03.oit.umn.edu",
+server "asr-coursefees-prd-app-04.oit.umn.edu",
   roles: %w[web app db prod]
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, "staging"
 
-server "asr-coursefees-qat-03.oit.umn.edu",
+server "asr-coursefees-qat-app-04.oit.umn.edu",
   roles: %w[web app db prod]
