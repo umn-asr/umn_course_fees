14641f8de06219f60ad3dcce7eeb30faa91c75a7
Tony Thomas <thoma127@umn.edu>
2021-05-28 09:54:26 -0500

    Match Gemfile in capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.16.0"
+lock "~> 3.16.0"
 
 set :application, "course-fees.umn.edu"
 set :repo_url, "git@github.umn.edu:asrweb/umn_course_fees.git"
