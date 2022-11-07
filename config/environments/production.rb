0a9c956e8e9dc1e5e03694290d150492aca01e7d
Tony Thomas <thoma127@umn.edu>
2022-11-07 09:15:09 -0600

    s: remove setting to hide deprecation warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,6 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Don't log any deprecations.
-  config.active_support.report_deprecations = false
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
