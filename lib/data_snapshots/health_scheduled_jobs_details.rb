0a1e1b8f75d7862fae5f4635e08ec7b77f63ee55
Ian Whitney <whit0694@umn.edu>
2023-09-12 12:57:52 -0500

    Update scheduled job health query
    Details behind this change are at https://github.umn.edu/asr-data-eng/asr_db_health/pull/15

--- lib/data_snapshots/health_scheduled_jobs_details.rb
+++ lib/data_snapshots/health_scheduled_jobs_details.rb
@@ -12,33 +12,50 @@ module DataSnapshots
     def self.definition_sql
       <<-SQL
         WITH schema_details AS (
-            SELECT user AS schema_name FROM DUAL
+            SELECT
+                user AS schema_name
+            FROM
+                dual
         ), environment_details AS (
-            SELECT SYS_CONTEXT('userenv','con_name') AS environment_name FROM DUAL
+            SELECT
+                sys_context('userenv', 'con_name') AS environment_name
+            FROM
+                dual
         )
         SELECT
-            schema_details.schema_name as schema
-            , environment_details.environment_name as environment
-            , 'health_scheduled_job_details' AS source
-            , JSON_OBJECT(
-                'job_name' is user_scheduler_jobs.job_name
-                , 'enabled' is user_scheduler_jobs.enabled
-                , 'log_date' is coalesce(user_scheduler_job_run_details.log_date, null)
-                , 'log_status' is coalesce(user_scheduler_job_run_details.status, 'never run')
-                , 'run_duration' is to_char(user_scheduler_job_run_details.run_duration,'HH:MM:SS')
-            ) AS message
-            , systimestamp as log_time
+            schema_details.schema_name                                      AS schema,
+            environment_details.environment_name                            AS environment,
+            '#{snapshot_name}'                                  AS source,
+            JSON_OBJECT(
+                    'job_name' IS user_scheduler_jobs.job_name,
+                    'enabled' IS user_scheduler_jobs.enabled,
+                    'log_date' IS coalesce(user_scheduler_job_run_details.log_date, NULL),
+                    'log_status' IS coalesce(user_scheduler_job_run_details.status, 'never run'),
+                    'run_duration' IS to_char(user_scheduler_job_run_details.run_duration, 'HH:MM:SS')
+                )
+            AS message,
+            coalesce(user_scheduler_job_run_details.log_date, systimestamp) AS log_time
         FROM
-          schema_details
-          , environment_details
-          , user_scheduler_jobs
-        LEFT JOIN user_scheduler_job_run_details ON
-          1 = 1
-          AND user_scheduler_jobs.job_name = user_scheduler_job_run_details.job_name
+            schema_details,
+            environment_details, user_scheduler_jobs
+            LEFT JOIN user_scheduler_job_run_details ON 1 = 1
+                                                        AND user_scheduler_jobs.job_name = user_scheduler_job_run_details.job_name
         WHERE
-          user_scheduler_job_run_details.log_date IS NULL
-          OR
-          user_scheduler_job_run_details.log_date >= TRUNC(sysdate)
+            ( user_scheduler_job_run_details.log_date IS NULL
+              OR user_scheduler_job_run_details.log_date >= (
+                SELECT
+                    from_tz(CAST(trunc(last_refresh_date, 'mi') AS TIMESTAMP),
+                            (
+                        SELECT
+                            to_char(systimestamp, 'TZR')
+                        FROM
+                            dual
+                    ))
+                FROM
+                    user_mviews
+                WHERE
+                    mview_name = '#{snapshot_name.upcase}'
+            ) )
       SQL
     end
   end
